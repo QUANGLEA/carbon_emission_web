79ced51c2cefd37ed1a7d3ef6bf42f272597842d
Quang Le <leanhquang2803@gmail.com>
2022-09-24 12:11:38 -0500

    Add initial slider

--- carbon_emission/components/Quiz.js
+++ carbon_emission/components/Quiz.js
@@ -1,9 +1,47 @@
+import Slider from "@mui/material/Slider";
+import { useState } from "react";
+
 export default function Quiz() {
+  const marks = [
+    {
+      value: 0,
+      label: "0°C",
+    },
+    {
+      value: 20,
+      label: "20°C",
+    },
+    {
+      value: 37,
+      label: "37°C",
+    },
+    {
+      value: 100,
+      label: "100°C",
+    },
+  ];
+
+  function valuetext(value) {
+    return `${value}°D`;
+  }
+
+  const [value, setValue] = useState();
   return (
     <div>
-      <h1>
-        Question 1: How much animal product did you consume in the past week?
-      </h1>
+      <div className="flex justify-center align-center text-center">
+        <div>
+          Question 1: How much animal product did you consume in the past week?
+        </div>
+        <br />
+        <Slider
+          aria-label="Custom marks"
+          defaultValue={20}
+          getAriaValueText={valuetext}
+          step={4}
+          valueLabelDisplay="auto"
+          marks={marks}
+        />
+      </div>
     </div>
   );
 }
