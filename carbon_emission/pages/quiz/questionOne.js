c9ca0b524fceccc548d99aa9c5b877d25852cecb
Quang Le <leanhquang2803@gmail.com>
2022-09-24 20:17:59 -0500

    Fix background image

--- carbon_emission/pages/quiz/questionOne.js
+++ carbon_emission/pages/quiz/questionOne.js
@@ -25,10 +25,7 @@ export default function QuestionOne() {
     "Meat consumption is responsible for releasing greenhouse gases such as methane, CO2, and nitrous oxide. These gases contribute to climate change, such as global warming. Other plant-based protein options, such as tempeh or seitan, are great ways of limiting your carbon footprint.";
 
   return (
-    <div
-      style="background-image: url(https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60)"
-      className="flex flex-col justify-center items-center h-screen w-screen bg-black text-white"
-    >
+    <div className="flex flex-col justify-center items-center h-screen w-screen bg-black text-white">
       <div className="grid grid-cols-3 grid-rows-3 gap-4 place-items-center">
         <Quiz
           question={question}
